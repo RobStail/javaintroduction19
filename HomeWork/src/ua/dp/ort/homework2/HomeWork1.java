2c17492a6de8465afcf6d6df22d0edfe9116ef2b
user1 <user1@Win10PC>
2019-11-17 20:49:02 +0200

    fixed text formatting

--- HomeWork/src/ua/dp/ort/homework2/HomeWork1.java
+++ HomeWork/src/ua/dp/ort/homework2/HomeWork1.java
@@ -4,11 +4,11 @@ public class HomeWork1 {
 
 	public static void main(String[] args) {
 
-        System.out.println("    *         \"\"\"    \\          /     \'\'\' ");
-    System.out.println("    *        \"  \"     \\        /     \'   \'");
-    System.out.println("    *       \"    \"     \\      /     \'     \'");
-    System.out.println("    *       \"\"\"\"\"\"      \\    /      \'\'\'\'\'\'\'  ");
-    System.out.println("*   *       \"    \"       \\  /       \'     \' ");
-    System.out.println(" ***        \"    \"        \\/        \'     \' ");  
-         }
+		System.out.println("    *         \"\"\"      \\          /      \'\'\'");
+		System.out.println("    *        \"   \"      \\        /     \'   \'");
+		System.out.println("    *       \"     \"      \\      /     \'     \'");
+		System.out.println("    *       \"\"\"\"\"\"\"       \\    /      \'\'\'\'\'\'\'  ");
+		System.out.println("*   *       \"     \"        \\  /       \'     \' ");
+		System.out.println(" ***        \"     \"         \\/        \'     \' ");
+	}
 }
