64e4e72e17780a51a5c94d3ff0ebc9e42afc3fa2
user1 <user1@Win10PC>
2019-11-29 21:20:12 +0200

    add homework 3

---
+++ HomeWork/src/ua/dp/ort/homework3/Varible.java
@@ -0,0 +1,51 @@
+package ua.dp.ort.homework3;
+
+public class Varible {
+	public static void main(String[] args) {
+	
+	byte dayweek = 7;// дней недели
+	byte love = 1; //любовь
+	byte wife = 1; //жена
+	short famely = 4;// семь€
+	short dayInYear = 365; //дней в году
+	short dayInMonth = 30; //дней мес€ца
+	int fanats = 5783992;// фанаты
+	int balls = 22123; //м€чи 
+	int people = 700200;//люди
+	long alfabets = 32; // алфавит
+	long breads =77783; //хлеб
+	long cats = 67882; //коты
+	float rating = 44.2f;// рейтинг
+	float salare = 33344.24f; //зарплата
+	float lengthOfCable = 883.2f; // длина кабел€
+	double lengthRoad = 124.4; //дина дороги
+	double distance = 4568.56; // дистанци€
+	double square = 582940.11;// площадь
+	char ot = '\u00A7';
+	char ii= '\u00B1';
+	char yy = '\u00A9';
+	
+	
+	System.out.println(dayweek);
+	System.out.println(love);
+	System.out.println(wife);
+	System.out.println(famely);
+	System.out.println(dayInYear);
+	System.out.println(dayInMonth);
+	System.out.println(fanats);
+	System.out.println(balls);
+	System.out.println(people);
+	System.out.println(alfabets);
+	System.out.println(breads);
+	System.out.println(cats);
+	System.out.println(rating);
+	System.out.println(salare);
+	System.out.println(lengthOfCable);
+	System.out.println(lengthRoad);
+	System.out.println(distance);
+	System.out.println(square);
+	System.out.println(ot);
+			System.out.println(ii);
+					System.out.println(yy);
+	}
+}
---
+++ HomeWork/src/ua/dp/ort/homework3/constants.java
@@ -0,0 +1,19 @@
+package ua.dp.ort.homework3;
+
+public class constants {
+	public static final char multiplication = '\u002A';
+	  public static final char subtraction = '\u002D';
+	  public static final char summation= '\u002B';
+	  public static final char division = '\u002F';
+	  public static final char equally = '\u003D';
+	public static void main(String[] args) {
+	  
+	  
+	  System.out.println(multiplication);
+	  System.out.println(subtraction);
+	  System.out.println(summation);
+	  System.out.println(division);
+	  System.out.println(equally);
+	  
+	}
+}
