d800fe8bd8b5a27c299437e34906bdb84d0a21a8
user1 <user1@Win10PC>
2020-01-08 21:20:23 +0200

    Add home work 6

--- HomeWork/src/ua/dp/ort/homework6/GasStation.java
+++ HomeWork/src/ua/dp/ort/homework6/GasStation.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class GasStation {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String user—onsumer;
+		String dataInput;
 		double volumeTank = 0;
 		double milliliters = 0;
 		double client = 0;
 
 		while (true) {
 			System.out.println("Input volume Tank :");
-			user—onsumer = scanner.nextLine();
-			double uncheckedvolumeTank = Double.parseDouble(user—onsumer);
+			dataInput = scanner.nextLine();
+			double uncheckedvolumeTank = Double.parseDouble(dataInput);
 			if (uncheckedvolumeTank < 1000) {
 				System.out.println("Your input is not corect");
 				continue;
@@ -26,11 +26,11 @@ public class GasStation {
 		while (true) {
 			System.out.println("Input quantity uploads milliliters: ");
 
-			user—onsumer = scanner.nextLine();
-			double uncheckedmilliliters = Double.parseDouble(user—onsumer);
+			dataInput = scanner.nextLine();
+			double uncheckedmilliliters = Double.parseDouble(dataInput);
 			if (uncheckedmilliliters <= 0) {
 				System.out.println("Your input is not corect");
-				break;
+				continue;
 			}
 
 			else {
@@ -40,22 +40,21 @@ public class GasStation {
 		}
 		while (true) {
 			System.out.println("Input desired volume amount of liters: ");
-			user—onsumer = scanner.nextLine();
-			double uncheckedclient = Double.parseDouble(user—onsumer);
+			dataInput = scanner.nextLine();
+			double uncheckedclient = Double.parseDouble(dataInput);
 			if (uncheckedclient <= 0) {
 				System.out.println("Your input is not corect");
-				break;
+				continue;
 			} else {
 				client = uncheckedclient * 1000;
 				break;
 			}
 		}
-		while (true) {
 
-			System.out.println("Volume fuel left: " + (volumeTank - client) + " milliliters");
-			System.out.println("Diesel fill dose: " + client / milliliters + " portion(s).");
-			break;
-		}
+		System.out.println("Volume fuel left: " + (volumeTank - client) + " milliliters");
+		System.out.println("Diesel fill dose: " + client / milliliters + " portion(s).");
+
+		scanner.close();
 
 	}
 }
