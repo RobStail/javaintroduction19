1e67a765d78214b8c79a1b0a33829a23a8f2050d
user1 <user1@Win10PC>
2020-01-22 20:29:05 +0200

    Add

---
+++ HomeWork/src/ua/dp/ort/homework7/ArrayHomeWork.java
@@ -0,0 +1,45 @@
+package ua.dp.ort.homework7;
+
+import java.util.Scanner;
+
+public class ArrayHomeWork {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] userInputNumbers = new int[20];
+		for (int counter = 0; counter < 20; counter++) {
+			System.out.print("Please input " + (counter + 1) + " number -> ");
+			String input = scanner.nextLine();
+
+			int userInput = Integer.parseInt(input);
+			userInputNumbers[counter] = userInput;
+		}
+		scanner.close();
+
+		int sum = 0;
+		int positiveNumber = 0;
+		int result = 0;
+
+		for (int counter = 0; counter < userInputNumbers.length; counter++) {
+			if (userInputNumbers[counter] % 2 == 0 && userInputNumbers[counter] >= 0) {
+//				System.out.print(userInputNumbers[counter] + " ");
+				sum += userInputNumbers[counter];
+				positiveNumber++;
+			}
+
+		}
+		System.out.println("");
+		System.out.println("Sum positive even elements: " + sum);
+		System.out.println("Arithmetic mean of positive array elements: " + sum/positiveNumber);
+		
+		
+		
+	}
+
+}
+//+1) В массиве, содержащем положительные и отрицательные целые числа, вычислить сумму четных положительных элементов.
+//+2) Найти среднее арифметическое положительных элементов массива
+//3) Найти два наименьших (минимальных) элемента массива
+//4) Поменять местами минимальный и максимальный элементы
+//5) Переставить элементы заданного массива в обратном порядке, то есть произвести реверс массива.
+//6) Разложить положительные и отрицательные числа по разным массивам
